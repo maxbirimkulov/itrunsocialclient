761813945958b7288e9385c787185d2cb79c610b
maxbirimkulov <maxbirimkulov@mail.ru>
2023-02-13 19:43:02 +0600

    first add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {Suspense} from "react";
+import {Route, Routes} from 'react-router-dom'
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home/Home";
+import Friends from "./pages/Friends/Friends";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import MyProfile from "./pages/MyProfile/MyProfile";
+import '../src/styles/style.scss'
+import './utils/i18n'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={'...Loading'}>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route path='' element={<Home/>}/>
+            <Route path='friends' element={<Friends/>}/>
+            <Route path='register' element={<Register/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='myprofile' element={<MyProfile/>}/>
+          </Route>
+        </Routes>
+    </Suspense>
   );
 }
 
---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {IoMdNotifications} from 'react-icons/io'
+import HeaderSearch from "./HeaderSearch";
+import noUser from '../../assets/noUser.png'
+import {BiChevronsDown} from 'react-icons/bi'
+import SwitchLang from "./SwitchLang/SwitchLang";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="container">
+                <nav className='header__nav'>
+                    <div className='header__left'>
+                        <h1 className='header__title'>IT-RUN web</h1>
+                        <HeaderSearch/>
+                    </div>
+                    <div className='header__right'>
+                        <span className='header__notif'>
+                            <IoMdNotifications/>
+                        </span>
+                        <SwitchLang/>
+                        <span className='header__user'>
+                            <img className='header__user-photo' src={noUser} alt="User photo not found"/>
+                            <span className='header__user-icon'>
+                                <BiChevronsDown/>
+                            </span>
+                        </span>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
