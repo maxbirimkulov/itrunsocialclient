e857fa097345e7339cb6e85b4f2be5940ab76b8a
Phantather <akdil.hstech@gmail.com>
2023-02-19 18:20:04 +0600

    add page for profile

--- src/components/HomeAside/HomeAside.jsx
+++ src/components/HomeAside/HomeAside.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom"
+import {BiUserCircle} from "react-icons/bi"
+import {HiOutlineUsers} from "react-icons/hi"
 
 const HomeAside = () => {
 
@@ -9,9 +11,11 @@ const HomeAside = () => {
         <aside className="aside">
             <ul className="aside__menu">
                 <li className="aside__item" onClick={() => navigate('/myprofile')}>
+                    <BiUserCircle/>
                     My profile
                 </li>
                 <li className="aside__item" onClick={() => navigate('/friends')}>
+                    <HiOutlineUsers/>
                     Friends
                 </li>
             </ul>
